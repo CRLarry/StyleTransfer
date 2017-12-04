f166478ac9c941f646744cc5b628c196d23558cb
Ubuntu <CRLarry@MiniBlazes.slgkqa1qsbfe5ccuwjjpf23lzd.bx.internal.cloudapp.net>
2017-12-04 20:14:36 +0000

    ruizhammock

--- Network.py
+++ Network.py
@@ -66,7 +66,7 @@ parser.add_argument("--style_scale", dest="style_scale", default=1.0, type=float
 parser.add_argument("--total_variation_weight", dest="tv_weight", default=8.5e-5, type=float,
                     help="Total Variation weight")
 
-parser.add_argument("--num_iter", dest="num_iter", default=40, type=int,
+parser.add_argument("--num_iter", dest="num_iter", default=50, type=int,
                     help="Number of iterations")
 
 parser.add_argument("--model", default="vgg19", type=str,
@@ -93,7 +93,7 @@ parser.add_argument("--init_image", dest="init_image", default="content", type=s
 parser.add_argument("--pool_type", dest="pool", default="max", type=str,
                     help='Pooling type. Can be "ave" for average pooling or "max" for max pooling')
 
-parser.add_argument('--preserve_color', dest='color', default="False", type=str,
+parser.add_argument('--preserve_color', dest='color', default="True", type=str,
                     help='Preserve original color in image')
 
 parser.add_argument('--min_improvement', default=0.0, type=float,
